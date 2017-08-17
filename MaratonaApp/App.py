ffc2f1aaa6fd24b88852936554340a2bc58b13f6
yohannatethysy <thetysy@gmail.com>
2017-08-16 22:22:45 -0300

    Add files via upload

---
+++ MaratonaApp/App.py
@@ -0,0 +1,18 @@
+from model.Pessoa import Pessoa
+from model.Cliente import Cliente
+from model.Funcionario import Funcionario
+from model.Maratona import Maratona
+
+def main():
+    cliente = Cliente('Xuxa')
+    funcionario = Funcionario('Beltrano')
+    maratona = Maratona()
+
+    maratona.correr(cliente)
+    maratona.correr(funcionario)
+
+    pessoa = Pessoa('Cicrana')
+    print(pessoa)
+
+if __name__ == "__main__":
+    main()
