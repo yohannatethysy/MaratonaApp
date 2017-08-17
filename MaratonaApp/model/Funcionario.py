2cf1ac4b1a2885bba4a45989a26ba6798d88a8de
yohannatethysy <thetysy@gmail.com>
2017-08-16 22:26:49 -0300

    Update Funcionario.py

--- MaratonaApp/model/Funcionario.py
+++ MaratonaApp/model/Funcionario.py
@@ -1,7 +1,5 @@
 from model.Pessoa import Pessoa
 
-class Funcionario():
-    def __init__(self, nome):
-        super(Funcionario, self).__init__(self, nome)
-        self.nome = nome
+class Funcionario(Pessoa):
+    pass
 
