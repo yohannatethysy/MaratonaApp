791992791610fb63c4bd24f689ee403f07efb416
yohannatethysy <thetysy@gmail.com>
2017-08-16 22:24:10 -0300

    Update Cliente.py

--- MaratonaApp/model/Cliente.py
+++ MaratonaApp/model/Cliente.py
@@ -3,8 +3,7 @@ from model.Pessoa import Pessoa
 
 class Cliente(Pessoa):
     def __init__(self, nome):
-        super(Cliente, self).__init__(self, nome)
-        self.nome = nome
+        super(Cliente, self).__init__(nome)
 
     def pagar(self):
         print('Pagando especificamente')
